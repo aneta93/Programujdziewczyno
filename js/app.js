bb1d4f1d5cedb32de2dcf1d940a897b36b7eea46
aneta93 <konta93@o2.pl>
2017-10-14 16:37:49 +0200

    zadanie 2

--- js/app.js
+++ js/app.js
@@ -1,25 +1,37 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function() {
 
-var forDropdown = document.querySelector('.for-dropdown');
-console.log('forDropdown');
+  var forDropdown = document.querySelector('.for-dropdown');
+  console.log('forDropdown');
 
-var menu = document.querySelector('.dropdown');
-console.log('menu');
+  var menu = document.querySelector('.dropdown');
+  console.log('menu');
 
-forDropdown.addEventListener('mouseover', function() {
-menu.style.display = 'block';
+  forDropdown.addEventListener('mouseover', function() {
+  menu.style.display = 'block';
+  });
+
+  forDropdown.addEventListener('mouseout', function() {
+  menu.style.display = 'none';
+  });
+
+//zad 2
+  var buttons = document.querySelectorAll('.read-more');
+  console.log(buttons);
+
+  var wiecej = document.querySelectorAll('.more');
+  console.log(wiecej);
+
+  console.log(buttons[0].previousElementSibling);
+
+  var this = document.querySelectorAll(buttons);
+  console.log(this);
+
+  buttons[0].addEventListener('click', function() {
+    console.log(this);
+  });
+
 });
-
-forDropdown.addEventListener('mouseout', function() {
-menu.style.display = 'none';
-});
-
-var wiecej = document.querySelectorAll('.btn btn-default read-more');
-console.log('wiecej');
-
-console.log(wiecej[0].previousElementSibling);
-   });
 
 
 
